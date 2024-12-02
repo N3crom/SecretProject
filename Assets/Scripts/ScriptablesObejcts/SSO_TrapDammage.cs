5206fb8cd5578014a411681181ad31c2453cda65
MartinsFlorian <f.martins2005.fm@gmail.com>
2024-12-02 12:01:23 +0100

    SSO_TrapDammage

---
+++ Assets/Scripts/ScriptablesObejcts/SSO_TrapDammage.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TrapDammage", menuName = "ScriptableObjects/Trap/TrapDammage")]
+public class SSO_TrapDammage : ScriptableObject
+{
+    public float dammage;
+}
